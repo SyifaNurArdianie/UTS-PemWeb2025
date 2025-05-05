f80b71d150512a6edabb6362a0726ecbe9b6ce54
SyifaNurArdianie <syifanrdianie@gmail.com>
2025-05-05 08:02:24 +0700

    Add files via upload

---
+++ fullsun6.php
@@ -0,0 +1,20 @@
+<?php
+    $angka1 = 15.9;
+    $angka2 = 7.9;
+
+    // Penjumlahan
+    $hasilTambah = $angka1 + $angka2;
+    echo "Penjumlahan (PHP): " . $hasilTambah . "<br>";
+
+    // Pengurangan
+    $hasilKurang = $angka1 - $angka2;
+    echo "Pengurangan (PHP): " . $hasilKurang . "<br>";
+
+    // Perkalian
+    $hasilKali = $angka1 * $angka2;
+    echo "Perkalian (PHP): " . $hasilKali . "<br>";
+
+    //Pembagian
+    $hasilBagi = $angka1 / $angka2;
+    echo "Pembagian (PHP): " . $hasilBagi . "<br>";
+    ?>
